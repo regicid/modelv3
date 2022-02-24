4d23a9a3e5c6623c16b20da3cee9a548dc40ea39
regicid <57593826+regicid@users.noreply.github.com>
2022-02-24 18:04:04 +0100

    bla

--- model.py
+++ model.py
@@ -7,18 +7,6 @@ import matplotlib.pyplot as plt
 import matplotlib
 from tqdm import tqdm
 import copy
-def norm_distrib(x,loc,scale=np.sqrt(1-r**2)*σ):
-    x = np.array(x)
-    loc = np.array(loc)
-    x = np.tile(x,(loc.size,1))
-    loc = np.tile(loc,(loc.size,1))
-    z = (scale/np.sqrt(2*np.pi))*np.exp(-(x-loc.T)**2/(2*scale**2))
-    return z/z.sum(1,keepdims=1)
-
-def probas(modif):
-    z = (norm_distrib(x = state_space,loc = r*state_space + (1-r)*u + modif,scale = np.sqrt(1-r**2)*σ))
-    return(z)
-
 
 def dyn_prog(T,p,v,prob_matrixes,n=10,r=.99,γ=1/3,m=.01,f2=.05,ω=.1,β=10,π=20,state_space = np.round(np.linspace(-50,50,1001),1)):
     p = (1-(1-p)**n)/n
@@ -44,7 +32,7 @@ def dyn_prog(T,p,v,prob_matrixes,n=10,r=.99,γ=1/3,m=.01,f2=.05,ω=.1,β=10,π=2
 
 
 class Population:
-    def __init__(self,μ,σ,N,prob_matrixes,T = 200,n=10,r=.99,γ=1/3,β=10,π=20,state_space = np.round(np.linspace(-50,50,1001),1),initial_v=0,update_rate = 1):
+    def __init__(self,μ,σ=4,N=10**5,T = 200,n=10,r=.99,γ=1/3,β=10,π=20,state_space = np.round(np.linspace(-50,50,1001),1),initial_v=0,update_rate = 1):
         self.μ = μ
         self.σ = σ
         self.N = N
@@ -52,14 +40,26 @@ class Population:
         self.T = T
         self.r = r
         self.β = β
-        self.prob_matrixes = prob_matrixes
         self.update_rate = update_rate
         self.state_space = state_space
         self.states = np.round(np.random.normal(loc=self.μ,scale=self.σ,size=self.N),1).clip(self.state_space.min(),self.state_space.max())
         self.strategies = np.zeros(self.N,dtype="int8")
         self.p = 0
         self.v = initial_v
-        strategies = dyn_prog(T,self.p,self.v,prob_matrixes)[2]
+        def norm_distrib(x,loc,scale=np.sqrt(1-r**2)*σ):
+            x = np.array(x)
+            loc = np.array(loc)
+            x = np.tile(x,(loc.size,1))
+            loc = np.tile(loc,(loc.size,1))
+            z = (scale/np.sqrt(2*np.pi))*np.exp(-(x-loc.T)**2/(2*scale**2))
+            return z/z.sum(1,keepdims=1)
+
+        def probas(modif):
+            z = (norm_distrib(x = state_space,loc = r*state_space + (1-r)*μ + modif,scale = np.sqrt(1-r**2)*σ))
+            return(z)
+        self.prob_matrixes = np.array([probas(0),probas(-10),probas(10),probas(-20)])
+
+        strategies = dyn_prog(T,self.p,self.v,self.prob_matrixes)[2]
         positions = ((self.states-state_space.min())/(state_space[1]-state_space[0])).round().astype('int')
         self.strategies = strategies[positions]
         z = self.strategies < 2
--- run_parallel.py
+++ run_parallel.py
@@ -1,17 +1,16 @@
 import pickle
 import numpy as np
-from model import *
-import multiprocessing as mtp
-MU = np.linspace(10,16,10)
+from model import Population
+import multiprocess as mtp
+MU = np.linspace(10,16,20)
 
 def get_pop(mu):
-        prob_matrixes = np.array([probas(0),probas(-10),probas(10),probas(-20)])
-        P = Population(mu,4,10**5,prob_matrixes,update_rate = .1)
+        P = Population(mu)
         P.round(10)
         pickle.dump(P,open(f"result_{mu}.pl","wb"))
 
-l = mtp.Pool(processes=10)
-runs = l.map_async(get_pop,MU)
+l = mtp.Pool(processes=20)
+l.map_async(get_pop,MU)
 l.close()
 l.join()
 Results = []
