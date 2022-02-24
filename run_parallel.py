354712cbe7b069305cb0e8cc7e9600be98aece87
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-02-24 19:05:49 +0100

    bla

--- run_parallel.py
+++ run_parallel.py
@@ -2,14 +2,15 @@ import pickle
 import numpy as np
 from model import Population
 import multiprocess as mtp
+import time
 MU = np.linspace(10,16,4)
 
 def get_pop(mu):
         P = Population(mu)
-        P.round(10)
+        P.round(20)
         return(P)
 
-l = mtp.Pool(processes=2)
+l = mtp.Pool(processes=10)
 runs = l.map_async(get_pop,MU)
 l.close()
 l.join()
